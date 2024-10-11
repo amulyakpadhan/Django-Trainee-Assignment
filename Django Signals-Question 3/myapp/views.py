a6ede63ac9a431f30fd75a8c37f6c575087a753f
Amulya Kumar Padhan <137890044+amulyakpadhan@users.noreply.github.com>
2024-10-11 23:13:27 +0530

    Update views.py

--- Django Signals-Question 3/myapp/views.py
+++ Django Signals-Question 3/myapp/views.py
@@ -2,28 +2,6 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.db import transaction
 
-# def create_user(request):
-#     try:
-#         with transaction.atomic():
-#             user = User.objects.create(username='test_user1', password='password123')
-#             print("User creation attempted.")
-#     except Exception as e:
-#         return HttpResponse(f"Error: {str(e)}")
-
-#     # Check if the user was actually created in the database
-#     user_exists = User.objects.filter(username='test_user').exists()
-
-#     if user_exists:
-#         print("User was created despite the signal handler error.")
-#         return HttpResponse("User was created despite the signal handler error.")
-#     else:
-#         print("User creation failed due to signal handler error, no user was created.")
-#         return HttpResponse("User creation failed due to signal handler error, no user was created.")
-
-from django.contrib.auth.models import User
-from django.http import HttpResponse
-from django.db import transaction
-
 def create_user(request):
     user_created = False
     error_message = ""
