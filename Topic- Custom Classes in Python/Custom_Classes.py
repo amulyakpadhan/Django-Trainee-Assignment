87a9ac099b41bf6d3134f8a6eeb3eb4c231014c9
Amulya Kumar Padhan <137890044+amulyakpadhan@users.noreply.github.com>
2024-10-11 23:12:25 +0530

    Update Custom_Classes.py to add comments

--- Topic- Custom Classes in Python/Custom_Classes.py
+++ Topic- Custom Classes in Python/Custom_Classes.py
@@ -4,11 +4,14 @@ class Rectangle:
         self.width = width
 
     def __iter__(self):
-        yield {'length': self.length}
-        yield {'width': self.width}
+        yield {'length': self.length} # Yield length in the required format
+        yield {'width': self.width}  # Yield width in the required format
 
+# Driver Code
 if __name__=='__main__':
+    # Create a Rectangle instance
     rect = Rectangle(20, 10)
 
+    # Iterate over the Rectangle instance
     for i in rect:
         print(i)
